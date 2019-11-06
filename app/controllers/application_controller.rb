120e34c986d7c9cd6c754068b3be41dbce224bd6
Karen V <karenvo@usc.edu>
2019-11-06 10:39:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
-require_relative 'app/models/team.rb'
-require_relative 'app/models/hero.rb'
+require_relative './models/team.rb'
+require_relative './models/hero.rb'
 
 class App < Sinatra::Base
 
